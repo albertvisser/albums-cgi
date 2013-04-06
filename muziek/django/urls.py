1e1a1414ecb9e988a2dc2d5b6d9dcf63e875bc3c
Albert Visser <albert@lemoncurry>
2013-04-06 17:58:32 +0200

    repo structuur herordend; files.rst aangepast

--- muziek/django/urls.py
+++ muziek/django/urls.py
@@ -1,27 +1,16 @@
 from django.conf.urls.defaults import *
 
-urlpatterns = patterns('pythoneer.muziek.views',
-    (r'^$',                                                             'index'),
-    (r'^(?P<soort>(album|live))/select/$',                              'select'),
-    (r'^(?P<soort>(album|live))/select/(?P<keuze>\w+)/(?P<sortorder>\w+)/$', 'select'),
-    (r'^(?P<soort>(album|live))/select/(?P<keuze>\w+)/(?P<selitem>\w+)/(?P<sortorder>\w+)/$', 'select'),
-    (r'^(?P<soort>(album|live))/act/(?P<item>\d+)/$',                    'sel_detail'),
-    (r'^(?P<soort>(album|live))/nieuw/$',                                'nieuw'),
-    (r'^(?P<soort>(album|live))/add/$',                                  'wijzig'),
-    (r'^(?P<soort>(album|live))/(?P<item>\d+)/$',                        'detail'),
-    (r'^(?P<soort>(album|live))/(?P<item>\d+)/(?P<keuze>\w+)/(?P<selitem>\w+)/(?P<sortorder>\w+)/$',                        'detail'),
-    (r'^(?P<soort>(album|live))/(?P<item>\d+)/edit/$',                   'wijzig'),
-    (r'^(?P<soort>(album|live))/(?P<item>\d+)/(?P<type>(track|opname))/nieuw/$', 'nieuw'),
-    (r'^(?P<soort>(album|live))/(?P<item>\d+)/(?P<type>(track|opname))/add/$',   'wijzig'),
-    (r'^(?P<soort>(album|live))/(?P<item>\d+)/(?P<type>(track|opname))/(?P<subitem>\d+)/(?P<actie>edit)/$',  'wijzig'),
-    (r'^(?P<soort>(album|live))/(?P<item>\d+)/(?P<type>(track|opname))/(?P<subitem>\d+)/update/$',  'wijzig'),
-    (r'^(?P<type>(track|opname))/(?P<subitem>\d+)/edit/$',              'wijzig'),
-    (r'^(?P<soort>artiest)/nieuw/$',                                     'nieuw'),
-    (r'^(?P<soort>artiest)/add/$',                                       'wijzig'),
-    (r'^artiest/(?P<actie>lijst)/$',                                     'artiest'),
-    (r'^artiest/(?P<item>\d+)/edit/$',                                'wijzig'),
-    ## (r'^artiest/(?P<actie>lijst)/edit/$',                                'wijzig'),
+
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    (r'^$',        'pythoneer.views.index'),
+    (r'^files/',   'pythoneer.views.viewdoc'),
+    (r'^muziek/',  include('pythoneer.muziek.urls')),
+    (r'^doctool/', include('pythoneer.doctool.urls')),
+
 
     # Uncomment this for admin:
-#     (r'^admin/', include('django.contrib.admin.urls')),
+    (r'^admin/(.*)', admin.site.root),
 )
